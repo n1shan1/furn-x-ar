462d27903451901242fa4cb632c3c5c9c3201bd8
Nishant <66475461+n1shan1@users.noreply.github.com>
2023-03-05 10:22:46 +0530

    third-commit
    Added navigation, new AR furniture models, and fixed camera and movement related bugs in the AR UI.

--- furn-x-ar/Assets/Common/Scripts/Vuforia/CameraSettings.cs
+++ furn-x-ar/Assets/Common/Scripts/Vuforia/CameraSettings.cs
@@ -1,11 +1,4 @@
-﻿/*===============================================================================
-Copyright (c) 2015-2018 PTC Inc. All Rights Reserved.
-
-Copyright (c) 2015 Qualcomm Connected Experiences, Inc. All Rights Reserved.
-
-Vuforia is a trademark of PTC Inc., registered in the United States and other
-countries.
-===============================================================================*/
+﻿
 using UnityEngine;
 using System.Collections;
 using Vuforia;
